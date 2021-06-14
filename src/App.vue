f6c4365ce90eb76ddfebfa3de1ff46f0fb862b2e
usman ali <usman.intellective@gmail.com>
2021-06-14 20:45:09 +0500

    employee form completed..axios call ready for hitting node js api :)

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,10 @@
   <img alt="Vue logo" src="./assets/logo.png">
   <employee-form msg="I Love Devops"></employee-form>
 </template>
-
 <script>
 
 
 import EmployeeForm from './components/EmployeeForm.vue'
-
 
 export default {
   name: 'App',
@@ -16,16 +14,3 @@ export default {
   }
 }
 </script>
-
-<style>
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-</style>
--- src/components/EmployeeForm.vue
+++ src/components/EmployeeForm.vue
@@ -1,21 +1,80 @@
-<template>
-    <div class="employee">
-        <p>{{msg}}</p>
-        <button type="button" class="btn btn-primary">Primary</button>
-    </div>
 
+
+<template>
+  <div class="employee">
+    <form class="row g-3" v-on:submit.prevent="saveData">
+      <div class="col-md-6">
+        <label for="firstName" class="form-label">First Name</label>
+        <input type="text" class="form-control" v-model="firstName" required/>
+      </div>
+      <div class="col-md-6">
+        <label for="lastName" class="form-label">Last Name</label>
+        <input type="text" class="form-control" v-model="lastName" required/>
+      </div>
+      <div class="col-md-6">
+        <label class="form-label">Designation</label>
+        <select class="form-select" v-model="desig" required>
+          <option selected value="">Choose...</option>
+          <option v-for="item in desigArray" :key="item">{{item}}</option>
+        </select>
+      </div>
+      <div class="col-md-6">
+        <label class="form-label">Programming Language</label>
+        <select class="form-select" v-model="fovProg" required>
+          <option selected value="">Choose...</option>
+          <option v-for="item in progLang" :key="item">{{item}}</option>
+        </select>
+      </div>
+      <div class="col-12">
+        <button type="submit" class="btn btn-secondary">Done</button>
+      </div>
+    </form>
+  </div>
 </template>
 
 
+
+
 <script>
-export  default {
+import axios from 'axios';
+export default {
+  name: "employee",
+  props: {
 
-name:'employee',
-props:{
-    msg:String
-}
+    msg: String,
+    
+  },
+  data:function (){
+      return{
+
+        desigArray:['UI/UX Engineer','Full Stack Developer','Devops Engineer', "Software Engineer"],
+        progLang:["Java","Node","C#","Python","R","Go","Java Script"],
+        firstName:'',
+        lastName:'',
+        desig:'',
+        fovProg:''
+
+      }
 
 
-}
+  },
 
+  methods:{
+
+      saveData:function(){
+
+        console.log(this.firstName+"\n"+this.lastName+"\n"+this.desig+"\n"+this.fovProg);
+
+        axios({
+        method:'post',
+        url:''
+        }).then((res)=>{
+        
+          console.log(res.data);
+
+        });
+      }
+
+  }
+};
 </script>
