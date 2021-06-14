0adf758df6c57cd326596f36d8ad9070f8fb186c
usman ali <usman.intellective@gmail.com>
2021-06-14 18:42:04 +0500

    bootstrap 5 configured

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,24 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Vue js Application Running on Docker"/>
+  <employee-form msg="I Love Devops"></employee-form>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+
+import EmployeeForm from './components/EmployeeForm.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    EmployeeForm
   }
 }
 </script>
 
 <style>
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -23,4 +27,5 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
 </style>
---
+++ src/components/EmployeeForm.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="employee">
+        <p>{{msg}}</p>
+        <button type="button" class="btn btn-primary">Primary</button>
+    </div>
+
+</template>
+
+
+<script>
+export  default {
+
+name:'employee',
+props:{
+    msg:String
+}
+
+
+}
+
+</script>
